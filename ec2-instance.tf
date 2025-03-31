6d55982890130ce30bcea96deb61b8bcb7170ec5
shankaruna007 <shankarunakara3@gmail.com>
2025-03-31 22:46:34 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,7 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 data "aws_subnet" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
   filter {
     name   = "default-for-az"
     values = ["true"]
@@ -11,13 +19,17 @@ data "aws_subnet" "default" {
 
 data "aws_security_group" "default" {
   filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
     name   = "group-name"
     values = ["default"]
   }
 }
 
 resource "aws_instance" "this" {
-  ami           = "ami-071226ecf16aa7d96"
+  ami           = "ami-0efc43a4067fe9a3e"
   instance_type = "t2.micro"
   subnet_id     = data.aws_subnet.default.id
   vpc_security_group_ids = [data.aws_security_group.default.id]
