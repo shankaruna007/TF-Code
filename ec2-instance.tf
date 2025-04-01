bbc41e23d8b8092eaa1a3fe53517af2725cf83e0
shankaruna007 <shankarunakara3@gmail.com>
2025-04-01 15:39:31 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,48 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
-  }
-}
-
-data "aws_security_group" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-  filter {
-    name   = "group-name"
-    values = ["default"]
-  }
-}
-
-resource "aws_instance" "this" {
-  ami           = "ami-0efc43a4067fe9a3e"
-  instance_type = "t2.micro"
-  subnet_id     = data.aws_subnet.default.id
-  vpc_security_group_ids = [data.aws_security_group.default.id]
-
-  tags = {
+resource "aws_instance" "Demo-Server" {
+    ami = "ami-0efc43a4067fe9a3e"
+    instance_type = "t2.micro" 
+  
+ tags = {
     Name = "MyEC2Instance"
-  }
-}
-
-output "instance_id" {
-  value = aws_instance.this.id
-}
-
-output "public_ip" {
-  value = aws_instance.this.public_ip
+  } 
 }
